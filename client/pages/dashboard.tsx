d40bc003e2c56b8e9934755df9d1143e5bd978cd
Ricardo Rincon <ricardo_rm25@hotmail.com>
2022-01-01 21:27:19 -0500

    NFTCard, fix issues

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NFTCard } from "../components";
 import { NFTKind, useNFTs } from "../hooks/useNFTs";
 export default function Dashboard() {
   const { isLoading, nfts, error } = useNFTs(NFTKind.CREATED);
@@ -7,44 +8,25 @@ export default function Dashboard() {
     return <h1 className="py-10 px-20 text-3xl">No created assets</h1>;
   }
 
-  const soldItems = nfts.reduce((acc, curr, index, arr) => {
-    if (curr.sold) {
-      acc.push(curr);
-      arr.splice(index, 1);
-    }
-    return acc;
-  }, []);
+  const unsold = nfts.filter((nft) => !nft.sold);
+  const sold = nfts.filter((nft) => nft.sold);
 
   return (
     <div className="p-4">
       <h2 className="text-2xl py-2">Created Assets</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-        {nfts.map((nft, i) => (
-          <div key={i} className="border shadow rounded-xl overflow-hidden">
-            <img src={nft.image} className="rounded" />
-            <div className="p-4 bg-black">
-              <p className="text-2xl font-bold text-white">
-                Price - {nft.price} ETH
-              </p>
-            </div>
-          </div>
+        {unsold.map((nft, i) => (
+          <NFTCard key={i} nft={nft} />
         ))}
       </div>
 
-      {soldItems.length > 0 && (
+      {sold.length > 0 && (
         <React.Fragment>
           <hr />
           <h2 className="text-2xl py-2">Sold Assets</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-            {soldItems.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="rounded" />
-                <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">
-                    Price - {nft.price} ETH
-                  </p>
-                </div>
-              </div>
+            {sold.map((nft, i) => (
+              <NFTCard key={i} nft={nft} />
             ))}
           </div>
         </React.Fragment>
